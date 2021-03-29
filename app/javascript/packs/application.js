fe4f5aa8fd47d6bfb4c3982247165dc7c0e85034
Spike Vinz Cruz <spikevinz@gmail.com>
2021-03-29 17:44:35 +0800

    add tailwind css and add style to index and new

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "stylesheets/application" // Add this line
+
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
